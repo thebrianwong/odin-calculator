24d3c9a3dd0d65de69bcb003a2060ee30b645dba
Brian Wong <brianw7218@gmail.com>
2022-08-24 18:18:47 -0700

    Add basic math operator functions

---
+++ script.js
@@ -0,0 +1,15 @@
+const add = (x,y) => {
+    return x + y;
+}
+
+const subtract = (x,y) => {
+    return x - y;
+}
+
+const multiply = (x,y) => {
+    return x * y;
+}
+
+const divide = (x,y) => {
+    return x / y;
+}
