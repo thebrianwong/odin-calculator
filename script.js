37e5adefc58bd3fef531d01b0159db0c7cb2219b
Brian Wong <brianw7218@gmail.com>
2022-08-24 22:01:17 -0700

    Create functionality to click on number buttons and display the buttons' values

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let displayValue = 0;
+
 const add = (x,y) => {
     return x + y;
 }
@@ -17,3 +19,20 @@ const divide = (x,y) => {
 const operate = (operatorFunction,x,y) => {
     return operatorFunction(x,y);
 }
+
+const addNumberClickers = () => {
+    const numberButtons = document.querySelectorAll(".number-button");
+    numberButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            displayValue = Number(button.innerText);
+            updateDisplayValue();
+        })
+    })
+}
+
+const updateDisplayValue = () => {
+    const displayValueContainer = document.querySelector(".display-container");
+    displayValueContainer.innerText = displayValue;
+}
+
+addNumberClickers();
